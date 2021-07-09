f24c482c470a1dafb3c1edcfa4c09c7a2f21a87d
Christopher Hillman <katie5five.5@gmail.com>
2021-07-09 16:51:32 +0200

    added neccasary components and bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
