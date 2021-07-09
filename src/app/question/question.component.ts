bd896ac8806d605045e5e97d8ff2e9b13765fa14
Christopher Hillman <katie5five.5@gmail.com>
2021-07-09 22:26:07 +0200

    questions interview component functions

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InterviewService } from '../interview.service';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-question',
@@ -8,13 +9,86 @@ import { InterviewService } from '../interview.service';
 })
 export class QuestionComponent implements OnInit {
 
+  constructor(public interviewService:InterviewService, public userService:UserServiceService) { }
+
   questions:any;
-  constructor(public interviewService:InterviewService) { }
+  questionNum:number = 0;
+  points:number = 0;
+  percentage:number = 0;
+  showTotal:boolean = false;
+  showAnswerValidation:boolean = false
+  quizComplete:boolean = false;
+  pass?:boolean;
 
   ngOnInit(): void {
-    this.interviewService.getQuestions().subscribe(questions=>this.questions = questions); 
+   this.interviewService.getQuestions().subscribe(questions=>this.questions = questions); 
+  }
+
+  get username():any{
+    return this.userService.username;
+  }
+
+  // run on submit form
+  questionComplete(questionRef: any, index:number): any {
+    let q = this.questions[index];
+
+    if(!questionRef.value.answer){
+      this.showAnswerValidation = true;
+      return;
+    }
+     this.markAnswer(questionRef, q);
+  }
+
+// mark answer to question as correct or incorrect
+  private markAnswer(questionRef: any, q: any) {
+    if (questionRef.value.answer === q.correctOption) {
+      q.correct = true;
+    }
+    this.showAnswerValidation = false;
+    this.questionNum += 1;
+    if(this.questionNum > this.questions.length -1){
+      this.checkResults();
+    }
   }
 
 
+// add up correct answers
+  checkResults():void{
+    for(let i=0; i < this.questions.length; i++){
+     if(this.questions[i].correct){
+      this.points += 1; 
+     }
+    }
+    this.checkResult();
+  }
+
+  // convert result to precentage
+  private checkResult() {
+    this.percentage = this.points / this.questions.length * 100;
+    this.showGrade()
+  }
+
+// Display the final result with pass or fail
+  private showGrade() {
+    if (this.questionNum >= this.questions.length) {
+      this.showTotal = true;
+
+      if (this.percentage > 60) {
+        this.pass = true;
+      } else {
+        this.pass = false;
+      }
+    }
+  }
+
+  //Return to last question
+  lastQuestion():void{
+    this.questionNum -= 1;
+  }
+
+  // remove validation warning on radio selectors when selected an option
+  selectedAnswer():void{
+    this.showAnswerValidation = false;
+  }
 
 }
