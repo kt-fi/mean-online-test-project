378a903aa147b96d37ce9b07a220a7bc34f9d9d5
Christopher Hillman <katie5five.5@gmail.com>
2021-07-11 02:08:01 +0200

    Changes hardcode questions

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -14,15 +14,15 @@ export class QuestionComponent implements OnInit {
   // Questions to load if problem with questions.json
   
   q1:Question = new Question( 1, 1, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q2:Question = new Question( 2, 2, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q3:Question = new Question( 3, 3, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q4:Question = new Question( 4, 4, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q5:Question = new Question( 5, 5, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q6:Question = new Question( 6, 6, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q7:Question = new Question( 7, 7, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q8:Question = new Question( 8, 8, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q9:Question = new Question( 9, 9, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
-  q10:Question = new Question( 10, 10, "What is the HTML Element to insert javascript?", "<HTML>", "<script>","<js>","<ts>","option2", false)
+  q2:Question = new Question( 2, 2, "What does HTML stand for?","Hyper Text Markup Language","Hyper TypeScript Marketing Line","Henry Top Makeup Line","Hopeful Type Made Loosly", "option1", false)
+  q3:Question = new Question( 3, 3, "Where is correct to place <script> tag in HTML?","<head>","<body>","<head> or <body>","<meta>","option3",false)
+  q4:Question = new Question( 4, 4, "What is the extension for typescript files?",".js",".ts",".type",".script", "option2", false)
+  q5:Question = new Question( 5, 5, "How do you create an alert box in JavaScript?", "alert()", "albert()", "warning()", "alertbox()","option1", false)
+  q6:Question = new Question( 6, 6, "What does !true mean?","true","undefined","null","false","option4", false)
+  q7:Question = new Question( 7, 7,  "which is the correct IF statement?", "if{1==2}", "if(i=1)then{}", "if(i===1){}", "none of the above","option3", false)
+  q8:Question = new Question( 8, 8, "How do we create a click event in TypeScript?","(click)","(clickMe)","[click]","onClick","option1",false)
+  q9:Question = new Question( 9, 9,  "What is the larges <H> tag in HTML?","H1","H2","H5","H6","option1", false)
+  q10:Question = new Question( 10, 10,  "What does 2 + 2 -1 make?","1","2","3","4","option3", false)
 
   backupQuestions:any[] = [this.q1, this.q2, this.q3, this.q4, this.q5, this.q6, this.q7, this.q8, this.q9, this.q10]
 
